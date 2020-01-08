fcaaa529bd37912f291e1922c312a65b6294475b
Robert Pavel <rspavel@lanl.gov>
2020-01-08 14:55:47 -0700

    First Draft of Module Generator Script

---
+++ utils/genModule.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Pull compiler from command line
+modcompiler=""
+spackcompiler=""
+namecompiler=""
+
+if [ "$1" == "gcc" ]; then
+	modcompiler="gcc/8.2.0"
+	spackcompiler="gcc@8.2.0"
+	namecompiler="gcc-8.2.0"
+else
+	echo "Unsupported Toolchain Requested. Currently supported list [gcc]"
+fi
+
+# Get variables from spack
+spackarch=`spack arch`
+spackmod="${SPACK_ROOT}/share/spack/modules/${spackarch}"
+
+# Install spackage
+#spack install flecsalemm-deps%${spackcompiler}
+
+# Generate module load commands
+spack module tcl loads  --dependencies flecsalemm-deps%gcc@8.2.0 | tee flecsalemm-deps-${namecompiler}
+
+# Prepend to module path
+sed -i "1s;^;prepend-path MODULE_PATH ${spackmod}\n;" flecsalemm-deps-${namecompiler}
+
+# Add compiler load
+sed -i "1s;^;module load ${modcompiler}\n;" flecsalemm-deps-${namecompiler}
+
+# And add Module shebang(?)
+sed -i "1s;^;#%Module\n;" flecsalemm-deps-${namecompiler}
+
