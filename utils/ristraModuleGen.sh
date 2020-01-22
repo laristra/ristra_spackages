f368607d3a2b3db03308ada861ab8ff905035e38
Robert Pavel <rspavel@lanl.gov>
2020-01-22 16:10:35 -0700

    Generalized and Fixed Errors

--- utils/ristraModuleGen.sh
+++ utils/ristraModuleGen.sh
@@ -4,11 +4,11 @@ display_usage() {
 	echo "This script must be passed three arguments:"
 	echo "- The Compiler Module"
 	echo "- The spack spec to install against"
-	echo "- And the suffix for the module file"
-	echo "e.g. ./flecsaleMMModuleGen.sh gcc/8.2.0 +caliper%gcc@8.2.0 gcc-8.2.0-withCaliper" 
+	echo "- And the name for the module file"
+	echo "e.g. ./ristraModuleGen.sh gcc/8.2.0 flecsalemm-deps+caliper%gcc@8.2.0 flecsalemm-deps-gcc-8.2.0-withCaliper" 
 } 
 
-if [ $# -le 3 ] 
+if [ $# -lt 3 ] 
 then
 	display_usage
 	exit 1
@@ -24,20 +24,20 @@ spackarch=`spack arch`
 spackmod="${SPACK_ROOT}/share/spack/modules/${spackarch}"
 
 # Install spackage
-spack install flecsalemm-deps${spackSpec}
+spack install ${spackSpec}
 
 # Generate module load commands
-spack module tcl loads  --dependencies flecsalemm-deps${spackSpec} | tee flecsalemm-deps-${modName}
+spack module tcl loads  --dependencies ${spackSpec} | tee ${modName}
 
 # Prepend to module path
-sed -i "1s;^;prepend-path MODULEPATH ${spackmod}\n;" flecsalemm-deps-${modName}
+sed -i "1s;^;prepend-path MODULEPATH ${spackmod}\n;" ${modName}
 
 # And clean up the module path to not overwhelm users with spack
-echo "remove-path MODULEPATH ${spackmod}" >> flecsalemm-deps-${modName}
+echo "remove-path MODULEPATH ${spackmod}" >> ${modName}
 
 # Add compiler load
-sed -i "1s;^;module load ${compiler}\n;" flecsalemm-deps-${modName}
+sed -i "1s;^;module load ${compiler}\n;" ${modName}
 
 # And add Module shebang(?)
-sed -i "1s;^;#%Module\n;" flecsalemm-deps-${modName}
+sed -i "1s;^;#%Module\n;" ${modName}
 
