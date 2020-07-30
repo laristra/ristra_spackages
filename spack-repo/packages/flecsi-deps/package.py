8ab5b6391d6455e41278782df48abd2f5081fa2d
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2020-07-30 13:30:29 -0600

    update flecsi hpx version

--- spack-repo/packages/flecsi-deps/package.py
+++ spack-repo/packages/flecsi-deps/package.py
@@ -51,11 +51,11 @@ class FlecsiDeps(BundlePackage):
     depends_on('legion build_type=Debug', when='backend=legion +debug_backend')
     depends_on('legion build_type=Release', when='backend=legion ~debug_backend')
 
-    depends_on('hpx@1.3.0 cxxstd=14 malloc=system', when='backend=hpx')
+    depends_on('hpx@1.4.1 cxxstd=17 malloc=system', when='backend=hpx')
     depends_on('hpx build_type=Debug', when='backend=hpx +debug_backend')
     depends_on('hpx build_type=Release', when='backend=hpx ~debug_backend')
 
-    depends_on('boost@1.70.0: cxxstd=14 +program_options')
+    depends_on('boost@1.70.0: cxxstd=17 +program_options')
     depends_on('metis@5.1.0:')
     depends_on('parmetis@4.0.3:')
     depends_on('googletest@1.8.1+gmock')
