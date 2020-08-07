daac36f5e8eae59f05ce04201b1b5e3a48753dce
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2020-08-07 14:20:39 -0600

    Update legion package.py

--- spack-repo/packages/legion/package.py
+++ spack-repo/packages/legion/package.py
@@ -7,7 +7,6 @@
 from spack import *
 from spack.pkg.builtin.legion import Legion
 
-
 class Legion(Legion):
     version('ctrl-rep-7', commit='7041e61e70a4a041bfee983f2aab9e061c5a6e61', preferred=True)
     version('ctrl-rep-6', commit='095be5c6e8d36a6ddb235fd079bc6e9b8d37baeb')
@@ -17,3 +16,4 @@ class Legion(Legion):
     version('ctrl-rep-2', commit='96682fd8aae071ecd30a3ed5f481a9d84457a4b6')
     version('ctrl-rep-1', commit='a03671b21851d5f0d3f63210343cb61a630f4405')
     version('ctrl-rep-0', commit='177584e77036c9913d8a62e33b55fa784748759c')
+
