06cf5841664c4decbd1830db0ec664cc5ed6e4e4
Robert Pavel <rspavel@lanl.gov>
2020-11-13 13:21:05 -0700

    What is the opposite of DRY?

--- spack-repo/packages/flecsalemm-deps/package.py
+++ spack-repo/packages/flecsalemm-deps/package.py
@@ -66,9 +66,8 @@ class FlecsalemmDeps(BundlePackage):
     depends_on('exodusii')
     depends_on('random123')
     depends_on('hypre')
-    depends_on('lua~shared', when='~shared_lua')
-    depends_on('lua+shared', when='+shared_lua')
-    depends_on('lua@5.3.5')
+    depends_on('lua@5.3.5~shared', when='~shared_lua')
+    depends_on('lua@5.3.5+shared', when='+shared_lua')
     depends_on('netcdf-c@4.7.0:')
     depends_on('portage-deps', when='+portage')
     # Not actually a flecaslemm-dep but related to fixing dependency/trilinos issues
