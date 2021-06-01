1cb534f904a2e41cb3dfba92152c47098126c8f5
Robert Pavel <rspavel@lanl.gov>
2021-06-01 16:21:10 -0600

    Updated MM Deps

--- spack-repo/packages/flecsalemm-deps/package.py
+++ spack-repo/packages/flecsalemm-deps/package.py
@@ -31,9 +31,7 @@ class FlecsalemmDeps(BundlePackage):
             description='Build FleCSI Tutorials')
     variant('paraview', default=False,
             description='Build Paraview Support')
-    variant('flecstan', default=False,
-        description='Build FleCSI Static Analyzer')
-    variant('cinch', default=False,
+    variant('external_cinch', default=False,
         description='Enable External Cinch')
     variant('int64', default=False,
         description='Enable Metis/ParMetis int64 Support')
@@ -46,13 +44,13 @@ class FlecsalemmDeps(BundlePackage):
     variant('tide', default=False,
             description='Build with tide support')
 
-    depends_on("flecsi-deps @1:1.9")
+    depends_on("flecsi @1:1.9")
     for b in ['mpi', 'legion', 'hpx']:
-        depends_on("flecsi-deps backend=%s" % b,
+        depends_on("flecsi backend=%s" % b,
             when="backend=%s" % b)
-    for v in ['debug_backend', 'doxygen', 'hdf5', 'caliper', 'graphviz', 'tutorial', 'flecstan', 'cinch']:
-        depends_on("flecsi-deps +%s" % v, when="+%s" % v)
-        depends_on("flecsi-deps ~%s" % v, when="~%s" % v)
+    for v in ['debug_backend', 'doxygen', 'hdf5', 'caliper', 'graphviz', 'tutorial', 'external_cinch']:
+        depends_on("flecsi-sp +%s" % v, when="+%s" % v)
+        depends_on("flecsi-sp ~%s" % v, when="~%s" % v)
 
 
     depends_on('pkgconfig', type='build')
