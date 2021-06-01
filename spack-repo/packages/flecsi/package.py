37d2cd73b9ee1bf2935ff41bc1e49acf6fe44b21
Robert Pavel <rspavel@lanl.gov>
2021-06-01 10:59:35 -0600

    Beginning Unification of FLecsi Spackages
    Beginning current unification of flecsi/flecsi, ristra support team, and
    flecsi-deps spackages to upstream and migrate to unified workflow to
    better support flecsi developers in addition to users

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -17,16 +17,12 @@ class Flecsi(CMakePackage):
        interfaces,and dependency closures.
     '''
     homepage = 'http://flecsi.org/'
-    git      = 'https://github.com/laristra/flecsi.git'
+    git      = 'https://github.com/flecsi/flecsi.git'
 
-    version('devel', branch='devel', submodules=False, preferred=False)
-    version('1', branch='1', submodules=False, preferred=False)
-    version('1.4', branch='1.4', submodules=False, preferred=True)
-    version('flecsph', branch='stable/flecsph', submodules=True, preferred=False)
+    version('develop', branch='devel', submodules=False)
+    version('1', git="https://github.com/laristra/flecsi.git", branch='1', submodules=False, preferred=False)
+    version('1.4', git="https://github.com/laristra/flecsi.git",  branch='1.4', submodules=False, preferred=True)
 
-    variant('build_type', default='Release',
-            values=('Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'),
-            description='The build type to build', multi=False)
     variant('backend', default='mpi', values=('serial', 'mpi', 'legion', 'hpx', 'charmpp'),
             description='Backend to use for distributed memory', multi=False)
     variant('debug_backend', default=False,
@@ -45,28 +41,93 @@ class Flecsi(CMakePackage):
             description='Enable coverage build')
     variant('hdf5', default=True,
             description='Enable HDF5 Support')
-    variant('caliper', default=False,
-            description='Enable Caliper Support')
+    variant('caliper_detail', default='none',
+            values=('none', 'low', 'medium', 'high'),
+            description='Set Caliper Profiling Detail', multi=False)
     variant('graphviz', default=False,
             description='Enable GraphViz Support')
     variant('tutorial', default=False,
             description='Build FleCSI Tutorials')
     variant('flecstan', default=False,
             description='Build FleCSI Static Analyzer')
-    variant('cinch', default=True,
+    variant('external_cinch', default=True,
             description='Enable External Cinch')
+    variant('kokkos', default=False,
+            description='Enable Kokkos Support')
+    variant('unit_tests', default=False,
+            description='Build with Unit Tests Enabled')
 
-    for b in ['mpi', 'legion', 'hpx']:
-        depends_on("flecsi-deps backend=%s" % b,
-            when="backend=%s" % b)
-    for v in ['debug_backend', 'doxygen', 'coverage', 'hdf5', 'caliper', 'graphviz', 'tutorial', 'flecstan', 'cinch']:
-        depends_on("flecsi-deps +%s" % v, when="+%s" % v)
-        depends_on("flecsi-deps ~%s" % v, when="~%s" % v)
+    # All Current FLecsi Releases
+    for level in ('low', 'medium', 'high'):
+        depends_on('caliper', when='caliper_detail=%s' % level)
+        depends_on('caliper@2.0.1~adiak', when='@:1.9 caliper_detail=%s' % level)
+    depends_on('graphviz', when='+graphviz')
+    depends_on('hdf5+mpi', when='+hdf5')
+    depends_on('metis@5.1.0:')
+    depends_on('parmetis@4.0.3:')
+
+    # Flecsi@1.x
+    depends_on('cmake@3.12:', when='@:1.9')
+    # Requires cinch > 1.0 due to cinchlog installation issue
+    depends_on('cinch@1.01:', type='build', when='+external_cinch @:1.9')
+    depends_on('mpi', when='backend=mpi @:1.9')
+    depends_on('mpi', when='backend=legion @:1.9')
+    depends_on('mpi', when='backend=hpx @:1.9')
+    depends_on('legion+shared+mpi', when='backend=legion @:1.9')
+    depends_on('legion+hdf5', when='backend=legion +hdf5 @:1.9')
+    depends_on('legion build_type=Debug', when='backend=legion +debug_backend @:1.9')
+    depends_on('hpx@1.4.1 cxxstd=17 malloc=system max_cpu_count=128', when='backend=hpx @:1.9')
+    depends_on('hpx build_type=Debug', when='backend=hpx +debug_backend @:1.9')
+    depends_on('boost@1.70.0: cxxstd=17 +program_options', when='@:1.9')
+    depends_on('googletest@1.8.1+gmock', when='@:1.9')
+    depends_on('hdf5+hl+mpi', when='+hdf5 @:1.9')
+    depends_on('python@3.0:', when='+tutorial @:1.9')
+    depends_on('doxygen', when='+doxygen @:1.9')
+    depends_on('llvm', when='+flecstan @:1.9')
+    depends_on('pfunit@3.0:3.99', when='@:1.9')
+    depends_on('py-gcovr', when='+coverage @:1.9')
+
+    # Flecsi@2.x
+    depends_on('cmake@3.15:', when='@2.0:')
+    depends_on('boost@1.70.0 cxxstd=17 +program_options +atomic +filesystem +regex +system', when='@2.0:')
+    depends_on('graphviz', when='+graphviz @2.0:')
+    depends_on('kokkos@3.2.00:', when='+kokkos @2.0:')
+    depends_on('legion@ctrl-rep-9:ctrl-rep-99',when='backend=legion @2.0:')
+    depends_on('legion+hdf5',when='backend=legion +hdf5 @2.0:')
+    depends_on('hdf5@1.10.7:',when='backend=legion +hdf5 @2.0:')
+    depends_on('hpx@1.3.0 cxxstd=17 malloc=system',when='backend=hpx @2.0:')
+    depends_on('kokkos@3.2.00:', when='+kokkos @2.0:')
+    depends_on('mpich@3.4.1', when='@2.0: ^mpich')
+    depends_on('openmpi@4.1.0', when='@2.0: ^openmpi')
+
+
 
     conflicts('+tutorial', when='backend=hpx')
-    # conflicts('+hdf5', when='backend=hpx')
+    # Flecsi@2: no longer supports serial or charmpp backends
+    conflicts('backend=serial', when='@2.0:')
+    conflicts('backend=charmpp', when='@2.0:')
+    # FLecsi@2: no longer expects to control how backend is built
+    conflicts('+debug_backend', when='@2.0:')
+    # Flecsi@2: No longer supports previous TPL related flags
+    conflicts('+minimal', when='@2.0:')
+    # Flecsi@2: no longer provides documentation variants
+    conflicts('+doxygen', when='@2.0:')
+    conflicts('+doc', when='@2.0:')
+    # Flecsi@2: no longer provides coverage variants
+    conflicts('+coverage', when='@2.0:')
+    # Flecsi@2: no longer provides tutorial variants
+    conflicts('+tutorial', when='@2.0:')
+    # Flecsi@2: no longer supports flecstan
+    conflicts('+flecstan', when='@2.0:')
+    # Flecsi@2: integrates cinch and no longer depends on external installs
+    conflicts('+external_cinch', when='@2.0:')
+    # Current Flecsi@:1.9 releases do not support kokkos
+    conflicts('+kokkos', when='@:1.9')
+    # Unit tests require flog support
+    conflicts('+unit_tests', when='~flog')
 
     def cmake_args(self):
+        #TODO: Add a big switch on version because of course
         spec = self.spec
         options = ['-DENABLE_OPENMP=ON',
                    '-DCXX_CONFORMANCE_STANDARD=c++17',
