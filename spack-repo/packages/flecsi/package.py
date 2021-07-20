f394542eaa909fa7d18ec47dcb0804fa610dd228
Robert Pavel <rspavel@lanl.gov>
2021-07-20 10:00:08 -0600

    Constraining Caliper for 1.x
    Constraining `~libdw` for caliper when @:1.9 as we require a specific
    version of caliper where this would be a conflict otherwise for that
    version. Unable to constrain for `@2:` as we do not know what caliper
    versions are needed

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -67,7 +67,7 @@ class Flecsi(CMakePackage, CudaPackage):
     # All Current FLecsi Releases
     for level in ('low', 'medium', 'high'):
         depends_on('caliper', when='caliper_detail=%s' % level)
-        depends_on('caliper@2.0.1~adiak', when='@:1.9 caliper_detail=%s' % level)
+        depends_on('caliper@2.0.1~adiak~libdw', when='@:1.9 caliper_detail=%s' % level)
     depends_on('graphviz', when='+graphviz')
     depends_on('hdf5+hl+mpi', when='+hdf5')
     depends_on('metis@5.1.0:')
